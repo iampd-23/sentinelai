edd408a0d17a0460bcd9648a8f83dc61c5706eab
Priyadarshan Sharma <iampd2305@gmail.com>
2026-09-12 21:58:10 +0530

    Initial commit

---
+++ src/main/java/com/sentinelai/SentinelaiApplication.java
@@ -0,0 +1,13 @@
+package com.sentinelai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelaiApplication.class, args);
+	}
+
+}
