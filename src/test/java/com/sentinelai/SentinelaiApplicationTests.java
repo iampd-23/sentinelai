cfd3345a99a89e83b4923a19bfa09be280554316
Priyadarshan Sharma <iampd2305@gmail.com>
2026-09-13 10:03:53 +0530

    configure project dependencies and tests

--- src/test/java/com/sentinelai/SentinelaiApplicationTests.java
+++ src/test/java/com/sentinelai/SentinelaiApplicationTests.java
@@ -2,12 +2,13 @@ package com.sentinelai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SentinelaiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
